e8424856db8aebe8423e9b949589e9d8c71ab2ed
Joshua Ward <jward@bluemodus.com>
2020-01-19 01:27:03 -0700

    add footer and styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Footer extends Component {
-		render() {
-		return(
-			<footer>{this.props.footerText}</footer>
-		)
-	}
+const Footer = () => {
+	const [ date, setDate ] = useState('');
+
+	return(
+		<footer className="footerMain">
+			<div className="footerMain-inner">
+				<div className="info">
+					<div className="logo">
+						Feed<span className="colorRed">r</span>.
+					</div>
+					<div className="address">
+						123 Crestwood Dr.<br />
+						Anyville, MA, 01325
+					</div>
+					<div className="phone">
+						(123) 654-9876
+					</div>
+					<div className="copyright">
+						&copy;{(new Date().getFullYear())} - All Rights Reserved
+					</div>
+				</div>
+				<div className="info">
+					<h4>Support</h4>
+					<div>Contact</div>
+					<div>Twitter</div>
+					<div>GitHub</div>
+				</div>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({keywords}) => (
 	<header className="headerMain">
-		<div className="logo">Feed<span className="colorYellow">r</span>.</div>
+		<div className="logo">Feed<span className="colorRed">r</span>.</div>
 		<div className="search">
 			<input id="search" placeholder="search" onChange={keywords} />
 			<label htmlFor="search">
